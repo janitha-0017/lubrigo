591851c8e68cd6d2e6e5189b8a20facdec8beed3
janitha-0017 <66233856+janitha-0017@users.noreply.github.com>
2021-03-19 11:06:32 +0530

    With CSV file saving method

--- ArduinoSensor.ino
+++ ArduinoSensor.ino
@@ -1,30 +1,33 @@
 #include <BH1750.h>
 #include <Wire.h>
 
+unsigned long int milli_time;
 
 
 BH1750 lightMeter;
 
 void setup() {
   Serial.begin(9600);
+  Serial.println("CLEARDATA");
   
   Wire.begin();
   
   lightMeter.begin();
 
-  Serial.println(F("BH1750 Test begin"));
+  Serial.println("LABEL,Computer Time,Time (Milli Sec.),Lux");
 }
 
 void loop() {
-  
+  milli_time = millis();
   float total = 0;
+  float lux = lightMeter.readLightLevel();
   for(int i=0; i<=5; i++){
-    float lux = lightMeter.readLightLevel();
     total = total+lux;
     delay(1000);    
   }
-  Serial.print("Light: ");
-  Serial.print(total/10);
-  Serial.println(" lx");
+  Serial.print("DATA,TIME,");
+  Serial.print(milli_time);
+  Serial.print(",");
+  Serial.println(total/10);
   
 }
