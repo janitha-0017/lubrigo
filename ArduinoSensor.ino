f511dd8f5cd179b2f4c4aeb30e4bc30dd35487b6
janitha-0017 <66233856+janitha-0017@users.noreply.github.com>
2021-03-19 15:01:06 +0530

    CSV file saving method
    divide by counter

--- ArduinoSensor.ino
+++ ArduinoSensor.ino
@@ -14,20 +14,24 @@ void setup() {
   
   lightMeter.begin();
 
-  Serial.println("LABEL,Computer Time,Time (Milli Sec.),Lux");
+  Serial.println("LABEL,Computer Time,Time (Milli Sec.),Intensity (Average Lux)");
 }
 
 void loop() {
   milli_time = millis();
   float total = 0;
-  float lux = lightMeter.readLightLevel();
+  float count = 0;
+  
   for(int i=0; i<=5; i++){
+    float lux = lightMeter.readLightLevel();
+    Serial.println(lux);
     total = total+lux;
+    count = count + 1;
     delay(1000);    
   }
   Serial.print("DATA,TIME,");
   Serial.print(milli_time);
   Serial.print(",");
-  Serial.println(total/10);
+  Serial.println(total/count);
   
 }
