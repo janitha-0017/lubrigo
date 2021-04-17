5b98b60540e6ef19d777dcdd62241100c3a94c2c
janitha-0017 <66233856+janitha-0017@users.noreply.github.com>
2021-04-17 11:26:13 +0530

    Loop

--- ArduinoSensor.ino
+++ ArduinoSensor.ino
@@ -2,7 +2,8 @@
 #include <Wire.h>
 
 unsigned long int milli_time;
-
+int ledRed = 13;
+int ledGreen = 12;
 
 BH1750 lightMeter;
 
@@ -11,10 +12,32 @@ void setup() {
   Serial.println("CLEARDATA");
   
   Wire.begin();
+  pinMode(ledRed, OUTPUT);
+  pinMode(ledGreen, OUTPUT);
   
   lightMeter.begin();
 
   Serial.println("LABEL,Computer Time,Time (Milli Sec.),Intensity (Average Lux)");
+  
+//  milli_time = millis();
+//  float total = 0;
+//  float count = 0;
+//  
+//  for(int i=0; i<=5; i++){
+//    float lux = lightMeter.readLightLevel();
+//    Serial.println(lux);
+//    total = total+lux;
+//    count = count + 1;
+//    delay(1000);
+//    digitalWrite(ledRed, HIGH);    
+//  }
+//  digitalWrite(ledRed, LOW);
+//  digitalWrite(ledGreen, HIGH);
+//  
+//  Serial.print("DATA,TIME,");
+//  Serial.print(milli_time);
+//  Serial.print(",");
+//  Serial.println(total/count);
 }
 
 void loop() {
@@ -27,8 +50,12 @@ void loop() {
     Serial.println(lux);
     total = total+lux;
     count = count + 1;
-    delay(1000);    
+    delay(1000);
+    digitalWrite(ledRed, HIGH);    
   }
+  digitalWrite(ledRed, LOW);
+  digitalWrite(ledGreen, HIGH);
+  
   Serial.print("DATA,TIME,");
   Serial.print(milli_time);
   Serial.print(",");
